fbaad4fa2196820704bd69856d725bd6438b138f
Kevin Abbott <kja@tamu.edu>
2019-09-11 19:49:04 -0500

    function created

--- differentdigits.cpp
+++ differentdigits.cpp
@@ -3,62 +3,20 @@
 using namespace std;
 
 int main(){
-	
-	int r, b, i;
-	int ones = 0;
-	int tens = 0;
-	int hundreds = 0;
-	int thousands = 0;
-	int counter = 0;
-	cout << "Enter numbers a <= b:" << endl;
-	cin >> a >> b;
-	counter = b - a + 1;
-	
-	
-	if((b < a) || (a < 0 || b < 0) || (b >= 10000)){ // invalid values for a and b //
-		cout << "Invalid input" << endl;
-		return 0;
+	if((b < a) || (a < 0 || b < 0) || (b > 10000)){ // invalid values for a and b //
+	cout << "Invalid input" << endl;
+	return 0;
 	}
-
-	else{ // inputs are valid for both a and b //
-		for(i = a; i <= b; i++){
-			if(i < 100){
-				tens = i / 10;
-				ones = i % 10;
-//				how many times does the value for both
-				//tens and ones occur?
-//				cout << tens << ones;
-				if(ones == tens)
-					counter --;
-			}
-			else if((i >= 100) && (i < 1000)){			
-				hundreds = i / 100;
-				tens = (i / 10) % 10;
-				ones = i % 10;
-				
-				if((hundreds == tens)|| (hundreds == ones) || (tens == ones)){
-					counter --;
-				}
-			}
-			else{
-				int thousands = 0;
-				int hundreds = 0;
-				int tens = 0;
-				int ones = 0;
-
-				thousands = i / 1000;
-				hundreds = (i / 100) % 10;
-				tens = (i / 10) % 10;
-				ones = i % 10;
-
-				if((thousands == hundreds) || (thousands == tens) || (thousands == ones) || (hundreds == tens) || (hundreds == ones) || (tens == ones)){
-					counter --;
-				}
-			}
+	int goodNum = 0;
+	for(int i = a; i <= b i++){
+		for(int j = 0; j <= 9; j++){
+			if(countDigitOccurances(i, j) != 1 && countDigitOccurances(i, j) != 0){
+				break;
+			} else if(j == 9)
+				goodNum ++;
 		}
-		cout << "There are " << counter << " valid numbers between " << a << " and " << b << endl;
-		return 0;
 	}
+	cout << "There are " << goodNum << " valid numbers between " << a << " and " << b << endl;	
 }
 
 
